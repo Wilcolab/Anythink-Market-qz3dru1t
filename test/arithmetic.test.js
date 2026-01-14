1b454da78725ec9a67a8ad386a90dde761526b78
Roman Kozak <romko.kozak@gmail.com>
2026-01-14 21:04:39 +0000

    Add unit tests for power operations in arithmetic module

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -94,6 +94,64 @@ describe('Arithmetic', function () {
     });
 
 // TODO: Challenge #1
+describe('Power', function () {
+    it('raises a positive integer to a positive integer power', function (done) {
+        request.get('/arithmetic?operation=power&operand1=2&operand2=3')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 8 });
+                done();
+            });
+    });
+    it('raises a positive integer to the power of zero', function (done) {
+        request.get('/arithmetic?operation=power&operand1=5&operand2=0')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 1 });
+                done();
+            });
+    });
+    it('raises zero to a positive integer power', function (done) {
+        request.get('/arithmetic?operation=power&operand1=0&operand2=5')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 0 });
+                done();
+            });
+    });
+    it('raises a positive integer to a negative integer power', function (done) {
+        request.get('/arithmetic?operation=power&operand1=2&operand2=-2')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 0.25 });
+                done();
+            });
+    });
+    it('raises a negative integer to an even power', function (done) {
+        request.get('/arithmetic?operation=power&operand1=-3&operand2=2')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 9 });
+                done();
+            });
+    });
+    it('raises a negative integer to an odd power', function (done) {
+        request.get('/arithmetic?operation=power&operand1=-2&operand2=3')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: -8 });
+                done();
+            });
+    });
+    it('rejects invalid power operation', function (done) {
+        request.get('/arithmetic?operation=power&operand1=2&operand2=foobar')
+            .expect(400)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ error: "Invalid operand2: foobar" });
+                done();
+            });
+    });
+});
  
 
     describe('Multiplication', function () {
